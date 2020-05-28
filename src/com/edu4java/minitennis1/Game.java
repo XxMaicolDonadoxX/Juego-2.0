3655a7ca4b621b5f53bcd4efe68946147863e4b8
XxMaicolDonadoxX <ajani816@gmail.com>
2020-05-27 19:00:30 -0500

    fix

---
+++ src/com/edu4java/minitennis1/Game.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ajani
+ */
+package com.edu4java.minitennis1;
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mini Tennis");
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+        }
+}
